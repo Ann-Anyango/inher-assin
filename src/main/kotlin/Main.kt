f2c4ee936f40a658e0739e0416eb14a9b1912929
Ann-Anyango <annmwangombe@gmail.com>
2023-03-08 07:57:40 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+fun main() {
+     var car=Car("Corolla","Lexus","Black",5)
+     car.carry(5)
+     car.carry(9)
+     car.identity()
+     var bus= Bus("Toyota","Scania","blue",60)
+     println(car.calculateParkingFees(4))
+     println(bus.maxTripFare(46.4))
+     println(bus.calculateParkingFees(4))
+
+
+
+}
+ open class Car( var model:String, var make:String, var color: String,var capacity:Int){
+     fun carry(people:Int){
+          var x= capacity
+          if (capacity>=people){
+               println( "Carrying $people passangers")
+          }
+          else{
+              println("Over capacity by $x people")
+          }
+     }
+     fun identity(){
+          println("I am a $color $make $model")
+     }
+    open  fun calculateParkingFees(hours:Int):Int{
+          var fees =hours*20
+          return fees
+
+     }
+
+}
+class Bus ( model: String, make:String, color: String, capacity: Int):Car(make,model,color,capacity){
+         fun maxTripFare(fare:Double):Double {
+              return fare * capacity
+
+         }
+
+     override fun calculateParkingFees(hours: Int): Int {
+         // return super.calculateParkingFees(hours)
+          var fees=hours*capacity
+          return fees
+
+     }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//Create a class Car with the following attributes: make, model, color, capacity.
+//It has these functions:
+//- carry(people: Int) : Prints out “Carrying $people passengers” if the
+//number of people is within its capacity else it prints out “Over capacity
+//by $x people” where x is the number of people exceeding its capacity
+//(3 points)
